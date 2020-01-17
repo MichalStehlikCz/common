b7fb33d8709b8fd5715afe8f86a0af106aeb8584
Michal Stehlik <stehlik@provys.com>
2020-01-17 18:00:20 +0100

    Preset Jackson formatters for JSON / XML conversion for Spring Framework

---
+++ spring/src/main/java/com/provys/common/spring/HttpMessageConvertorFactory.java
@@ -0,0 +1,29 @@
+package com.provys.common.spring;
+
+import com.ctc.wstx.api.WstxOutputProperties;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+
+@Configuration
+class HttpMessageConvertorFactory {
+
+    private static final Logger LOG = LogManager.getLogger(HttpMessageConvertorFactory.class);
+
+    @Bean
+    public MappingJackson2XmlHttpMessageConverter provysJackson2XmlHttpMessageConverter(
+            Jackson2ObjectMapperBuilder builder) {
+        LOG.debug("Customize Jackson XML mapper");
+        XmlMapper mapper = builder.createXmlMapper(true).build();
+        mapper.enable(ToXmlGenerator.Feature.WRITE_XML_DECLARATION);
+        mapper.getFactory()
+                .getXMLOutputFactory()
+                .setProperty(WstxOutputProperties.P_USE_DOUBLE_QUOTES_IN_XML_DECL, true);
+        return new MappingJackson2XmlHttpMessageConverter(mapper);
+    }
+}
---
+++ spring/src/main/java/com/provys/common/spring/ProvysJacksonObjectMapperBuilderCustomizer.java
@@ -0,0 +1,32 @@
+package com.provys.common.spring;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(-5)
+class ProvysJacksonObjectMapperBuilderCustomizer implements Jackson2ObjectMapperBuilderCustomizer {
+
+    private static final Logger LOG = LogManager.getLogger(ProvysJacksonObjectMapperBuilderCustomizer.class);
+
+    @Override
+    public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+        LOG.debug("Use PROVYS Object Mapper customizer");
+        jacksonObjectMapperBuilder.annotationIntrospector(
+                AnnotationIntrospector.pair(new JacksonAnnotationIntrospector(),
+                        new JaxbAnnotationIntrospector(TypeFactory.defaultInstance())));
+        jacksonObjectMapperBuilder.modules(new Jdk8Module(), new JavaTimeModule());
+        jacksonObjectMapperBuilder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
+    }
+}
